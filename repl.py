79af81d08a18d2c89e68c9d54e347eb3e1970f9a
vincent <vincentcharlebois@gmail.com>
2025-01-13 23:01:21 -0500

    Add instructions and print statement for local testing

--- repl.py
+++ repl.py
@@ -4,6 +4,21 @@ import asyncio
 
 from roollm import (RooLLM, ROLE_USER, make_ollama_inference)
 
+"""
+This script allows local testing of the RooLLM class.
+You can chat with RooLLM in a terminal session.
+
+Usage:
+1. Create an .env file in the roollm directory with the following content:
+    
+    ROO_LLM_AUTH_USERNAME=
+    ROO_LLM_AUTH_PASSWORD=
+
+    Ask the RooLLM team for the credentials.
+
+2. Run this script to start the REPL.
+"""
+
 inference = make_ollama_inference()
 roo = RooLLM(inference)
 history = []
@@ -20,6 +35,8 @@ async def main():
         query = input(">")
         response = await roo.chat(user, query, history)
 
+        print(response['content'])
+        
         history.append({
             'role': ROLE_USER,
             'content': query
