cd05c08a49345c58654a9d62ca3971490403cd4d
Mauve Signweaver <contact@mauve.moe>
2025-01-15 17:06:16 -0500

    Add example of exponentials to calc tool

--- tools/calc.py
+++ tools/calc.py
@@ -2,7 +2,7 @@ import operator as op
 import ast
 
 name = 'calc'
-description = 'Execute some simple math. E.g. 4 * (20 / 6^9)'
+description = 'Execute some simple math. E.g. 4 * (20 / 6^9) ** 0.5'
 parameters = {
     'type': 'object',
     'properties': {
