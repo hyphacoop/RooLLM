004c61aa0a966627cedd449876385b96b015f199
vincent <vincentcharlebois@gmail.com>
2025-04-23 14:23:25 -0400

    fix: stop hardcoding MCP adapter path lol

--- mcp_config.py
+++ mcp_config.py
@@ -1,13 +1,23 @@
-import pathlib
 import sys
+import pathlib
 
-script_path = pathlib.Path(__file__).parent / "run_mcp_stdio.py"
+here = pathlib.Path(__file__).parent
+
+# are we running this locally from roollm/ or as part of hyphadevbot. zipped or installed?
+in_dev_mode = (here / "run_mcp_stdio.py").exists()
+
+if in_dev_mode:
+    command = sys.executable
+    args = [str((here / "run_mcp_stdio.py").resolve())]
+else:
+    command = sys.executable
+    args = ["-m", "hyphadevbot.roollm.run_mcp_stdio"]
 
 MCP_CONFIG = {
     "mcp_adapters": {
         "minima": {
-            "command": sys.executable,  
-            "args": [str(script_path.resolve())],
+            "command": command,
+            "args": args,
             "env": {
                 "MCP_ADAPTER": "minima_adapter.MinimaRestAdapter"
             }
