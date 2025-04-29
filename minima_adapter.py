835b70f401db8ba622ada4f7bcecf6b536f0dd49
vincent <vincentcharlebois@gmail.com>
2025-04-29 19:40:28 -0400

    Add emoji field to tool descriptions in MinimaRestAdapter

--- minima_adapter.py
+++ minima_adapter.py
@@ -281,7 +281,8 @@ class MinimaRestAdapter:
             {
                 "name": name,
                 "description": meta.get("description", ""),
-                "parameters": meta.get("parameters", {})
+                "parameters": meta.get("parameters", {}),
+                "emoji": meta.get("emoji")
             }
             for name, meta in self.tools.items()
         ]
