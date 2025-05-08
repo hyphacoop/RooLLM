5379ecc1ef9ff7e3c12dc27e497ae1762e3e5d2c
vincent <vincentcharlebois@gmail.com>
2025-05-08 11:54:44 -0400

    use absolute imports

--- __init__.py
+++ __init__.py
@@ -1,21 +1,11 @@
-try:
-    from bridge import MCPLLMBridge
-    from llm_client import LLMClient
-    from tools.tool_registry import ToolRegistry, Tool
-    from mcp_client import MCPClient
-    from github_app_auth import GitHubAppAuth, prepare_github_token
-    from minima_adapter import MinimaRestAdapter
-    from tools.local_tools_adapter import LocalToolsAdapter
-    from roollm import RooLLM
-except ImportError:
-    from .bridge import MCPLLMBridge
-    from .llm_client import LLMClient
-    from .tools.tool_registry import ToolRegistry, Tool
-    from .mcp_client import MCPClient
-    from .github_app_auth import GitHubAppAuth, prepare_github_token
-    from .minima_adapter import MinimaRestAdapter
-    from .tools.local_tools_adapter import LocalToolsAdapter
-    from .roollm import RooLLM
+from .bridge import MCPLLMBridge
+from .llm_client import LLMClient
+from .tools.tool_registry import ToolRegistry, Tool
+from .mcp_client import MCPClient
+from .github_app_auth import GitHubAppAuth, prepare_github_token
+from .minima_adapter import MinimaRestAdapter
+from .tools.local_tools_adapter import LocalToolsAdapter
+from .roollm import RooLLM
 
 __all__ = [
     'MCPLLMBridge',
